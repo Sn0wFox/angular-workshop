98c0b84eb07b50a9e4c928bdc2c83731c4741623
Sn0wFox <ruben.pericas@gmail.com>
2017-11-21 11:04:36 +0100

    Add pokemon component exemple

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CoverComponent } from './cover/cover.component';
 import { SearchComponent } from './search/search.component';
+import { PokemonComponent } from './pokemon/pokemon.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CoverComponent,
-    SearchComponent
+    SearchComponent,
+    PokemonComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pokemon/pokemon.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon',
+  templateUrl: './pokemon.component.html',
+  styleUrls: ['./pokemon.component.scss']
+})
+export class PokemonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  @Input() name: string;
+  @Input() id: number;
+
+}
